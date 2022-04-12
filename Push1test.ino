c69994f1e0addb30207ca6e38526c79b768c827e
JLaporteClass <103061836+JLaporteClass@users.noreply.github.com>
2022-04-12 11:46:41 -0700

    Update Push1test.ino

--- Push1test.ino
+++ Push1test.ino
@@ -2,6 +2,8 @@
 
 #include "DHT.h"
 
+#include <RTClib.h>
+
 LiquidCrystal lcd(10, 9, 7, 6, 5, 4);
 
 int Contrast = 60;
@@ -12,6 +14,7 @@ float humidIn = 0;
 #define DHTPIN 2
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
+RTC_DS3231 rtc;
 
 int resval = 0;  // holds the value
 int respin = A5; // sensor pin used
@@ -21,9 +24,17 @@ void setup()
   analogWrite(11, Contrast);
   lcd.begin(16,2);
   dht.begin();
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  
 }
 
 void loop() {
+    if (! rtc.begin()) {
+  lcd.display();
+  lcd.print("Couldn't find RTC");
+  lcd.clear();
+}
+    
     resval = analogRead(respin);
     lcd.display();
     if  (resval<=100) {lcd.print("Water Lvl: Empty");} 
@@ -48,5 +59,26 @@ void loop() {
     lcd.print(humidIn);
     delay(1000);
     lcd.clear();
-}
 
+    lcd.display();
+    lcd.setCursor(0,0);
+    DateTime now = rtc.now();
+    lcd.print("Date & Time: ");
+    lcd.setCursor(0,1);
+    lcd.print(now.year(), DEC);
+    lcd.print('/');
+    lcd.print(now.month(), DEC);
+    lcd.print('/');
+    lcd.print(now.day(), DEC);
+    lcd.print(" (");
+    lcd.print(now.dayOfTheWeek());
+    lcd.print(") ");
+    lcd.print(now.hour(), DEC);
+    lcd.print(':');
+    lcd.print(now.minute(), DEC);
+    lcd.print(':');
+    lcd.println(now.second(), DEC);
+    lcd.clear();
+ 
+  delay(1000);
+}
