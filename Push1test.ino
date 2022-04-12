410e9e64e970f3722e10f24dd35eb7e238b3403a
nimishasabu <66708581+nimishasabu@users.noreply.github.com>
2022-04-12 11:54:45 -0700

    Update Push1test.ino

--- Push1test.ino
+++ Push1test.ino
@@ -1,7 +1,9 @@
+#include <DS3231.h>
 #include <LiquidCrystal.h>
 
 #include "DHT.h"
 
+DS3231  rtc(SDA, SCL);
 LiquidCrystal lcd(10, 9, 7, 6, 5, 4);
 
 int Contrast = 60;
@@ -21,6 +23,8 @@ void setup()
   analogWrite(11, Contrast);
   lcd.begin(16,2);
   dht.begin();
+  rtc.begin(); // Initialize the rtc object
+  lcd.begin(16,2);
 }
 
 void loop() {
@@ -48,5 +52,15 @@ void loop() {
     lcd.print(humidIn);
     delay(1000);
     lcd.clear();
+  
+ lcd.setCursor(0,0);
+ lcd.print("Time:  ");
+ lcd.print(rtc.getTimeStr());
+ 
+ lcd.setCursor(0,1);
+ lcd.print("Date: ");
+ lcd.print(rtc.getDateStr());
+ 
+ delay(1000);
 }
 
