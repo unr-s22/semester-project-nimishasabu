fc9d5039961770454a1441ab8881cb02926b1cb6
nimishasabu <66708581+nimishasabu@users.noreply.github.com>
2022-04-26 19:26:31 -0700

    Create Push1test.ino

--- Push1test.ino
+++ Push1test.ino
@@ -1,128 +1,136 @@
-//4/21/21 01:08pm
+//4/26/21 07:22 pm
 #include <DS3231.h>
 #include <LiquidCrystal.h>
 #include <Stepper.h>
+#include <DHT.h>
 
-#include "DHT.h"
+#define DHTPIN 6
+#define DHTTYPE DHT11
 
 DS3231  rtc(SDA, SCL);
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7); 
 Stepper myStepper(512, 2,3,4,5);
+DHT dht(DHTPIN, DHTTYPE);
 
+//const int fan = 45; volatile const int yLED = 53; B0
+//volatile const int bLED = 51; B2 volatile const int rLED = 47; L2 volatile const int gLED = 49; L0
+unsigned char *port_B = (unsigned char *) 0x25;  // for yLED and bLED
+unsigned char *DDR_B = (unsigned char *) 0x24;
+unsigned char *port_L = (unsigned char *) 0x10B; // for fan rLED and gLED
+unsigned char *DDR_L = (unsigned char *) 0x10A;
+//unsigned char *port_D = (unsigned char *) 0x2B;
+//unsigned char *DDR_D = (unsigned char *) 0x2A;
 int Contrast = 60;
 float tempIn = 0;
 float tempF = 0;
 float humidIn = 0;
-int state;
+int disabledState = 1;
 
-#define DHTPIN 6
-#define DHTTYPE DHT11
-DHT dht(DHTPIN, DHTTYPE);
-
-int resval = 0;  // holds the value
-int respin = A5; // sensor pin used
+int resval = 0;                  // holds the sensor value
+int respin = A5;                 // sensor pin used
 const int stepButton = 53;
 int stepButtonState = 0;
-const int fan = A4;
-int fanState = 0;
+int steppos = 0;
 volatile int onOffButton = 18;
 volatile int resetButton = 3;
-volatile int yLED = 51;
-const int bLED = 53;
-const int rLED = 47;
-const int gLED = 49;
+int resetButtonState = 0;
+void disabled();
+
 
 void setup()
 {
   analogWrite(13, Contrast);
   lcd.begin(16,2);
   dht.begin();
-  rtc.begin(); // Initialize the rtc object
+  rtc.begin();                         // Initialize the rtc object
   rtc.setTime(12,34,30);
   rtc.setDate(4,12,2022);
   myStepper.setSpeed(200);
   pinMode(stepButton, INPUT);
   pinMode(onOffButton, INPUT_PULLUP);
   pinMode(resetButton, INPUT);
-  pinMode(yLED, OUTPUT);
-  pinMode(bLED, OUTPUT);
-  pinMode(gLED, OUTPUT);
-  pinMode(rLED, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(onOffButton), disabled, LOW);
+
+  *DDR_B |= 0b00000001 | 0b00000100;   //yled | bled - output ;
+  *DDR_L |= 0b00010000 | 0b00000001 | 0b00000100; //fan | gled | rled - output;
+ 
+  attachInterrupt(digitalPinToInterrupt(onOffButton), disabled, FALLING);
 }
 
 void loop() {
-  digitalWrite(yLED, HIGH);
-  digitalWrite(gLED, HIGH);
-  digitalWrite(rLED, HIGH);
-  digitalWrite(bLED, HIGH);
+  //*port_B &= (0 << PINB0) & (0 << PINB2); // set yled and bled to low
+  //*port_L &= (0 << PINL4) & (0 << PINL0) & (0 << PINL2); // set fan gled rled to low
+  
   // Water Sensor
-
-  if (state==0){
-    digitalWrite(yLED, LOW);
+  if (disabledState==1){
+    *port_B = (1 << PINB0); // digitalWrite(yLED, HIGH);
+    *port_L &= (0 << PINL4);
     lcd.noDisplay();
-    fanState = 0;
   }
   else {
-    lcd.clear();
-    lcd.display();
-    lcd.setCursor(0,0);
-    if  (resval<=100) {lcd.print("Water Lvl: Empty");} 
-    else if (resval>100 && resval<=300) {lcd.print("Water Lvl: Lw"); } 
-    else if (resval>300 && resval<=330) {lcd.print("Water Lvl: Md"); } 
-    else if (resval>330) {lcd.print("Water Lvl: Full"); }
-    delay(1000); 
-    lcd.clear();
+    *port_B &= (0 << PINB0);
+    *port_L = 0b00010001; //(1 << PINL0) | (1 << PINL4);
 
-  // Read and display Temp and Humid. Determine state run and idle
-  humidIn = dht.readHumidity();
-  tempIn = dht.readTemperature();
-  tempF = dht.readTemperature(true);
-  lcd.setCursor(0,0);
-  lcd.print("Temp: ");
-  lcd.setCursor(7,0);
-  lcd.print(tempF);
-  delay(1000);
-  lcd.setCursor(0,1);
-  lcd.print("Humid: ");
-  lcd.setCursor(8,1);
-  lcd.print(humidIn);
-  delay(1000);
-  lcd.clear(); 
 
-  // Stepper Motor, should move independent of rest, not when disabled
-  int stepButtonState = digitalRead(stepButton);
-  if (stepButtonState == HIGH){myStepper.step(100);}
-  delay(1000);
-  lcd.clear();
+  }
+    
+//     resval = analogRead(respin);
+//     
+//     // Error state
+//     while (resval<=100 || resetButtonState != 0) {
+//      digitalRead(resetButton);
+//      lcd.clear();
+//      lcd.display();
+//      lcd.setCursor(0,0);
+//      *port_L |= (0 << PINL2); // digitalWrite(rLED, HIGH);
+//      lcd.print("Water Lvl: Empty"); 
+//      delay(1000);
+//      lcd.clear();
+     
+//
+//    // Read and display Temp and Humid. Determine state run and idle
+//    humidIn = dht.readHumidity();
+//    tempIn = dht.readTemperature();
+//    tempF = dht.readTemperature(true);
+//    lcd.setCursor(0,0);
+//    lcd.print("Temp: ");
+//    lcd.setCursor(7,0);
+//    lcd.print(tempF);
+//    delay(1000);
+//    
+//    // state change to idle from running
+//    if (tempIn < 80.0) {
+//      state = 2;
+//    }
+//    
+//    lcd.setCursor(0,1);
+//    lcd.print("Humid: ");
+//    lcd.setCursor(8,1);
+//    lcd.print(humidIn);
+//    delay(1000);
+//    lcd.clear(); 
+//
+//    // Stepper Motor, should move independent of rest, not when disabled
+//    int stepButtonState = digitalRead(stepButton);
+//    if (stepButtonState == HIGH){myStepper.step(100); steppos += 1;}
+//    delay(1000);
+//    lcd.clear();
+
 }
 
 void disabled() {
   // YELLOW LED ON, FAN OFF, No sensors
-  digitalWrite(yLED, LOW);
-  lcd.noDisplay();
+  if (disabledState == 0) {disabledState = 1;}
+  else {disabledState = 0;}
 }
 
- void idle(){
-  //GREEN LED ON, CURRENT TIME, CURRENT WATER LEVEL 
-  digitalWrite(gLED, LOW);
-}
-
-void Times(){
-  // During state transition, send this date and time
-  lcd.setCursor(0,0);
-  lcd.print("Time:  ");
-  lcd.println(rtc.getTimeStr());
-  lcd.setCursor(0,1);
-  lcd.print("Date: ");
-  lcd.print(rtc.getDateStr());
-  delay(1000);
-
-}
-
-void error(){
-  //RED LED ON, ERROR MESSAGE, Motor off
-  digitalWrite(rLED,LOW);
-}
-
-}
+//void Times(){
+//  // During state transition, send this date and time
+//  Serial.print("Time:  ");
+//  Serial.println(rtc.getTimeStr());
+//  Serial.print("Date: ");
+//  Serial.println(rtc.getDateStr());
+//  Serial.print("Vent Position: ");
+//  Serial.print(steppos);
+//  delay(1000);
+//}
+//
