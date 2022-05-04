0fb185acaf6b8e18d1cfdd71e25e6dca1e584389
JLaporteClass <103061836+JLaporteClass@users.noreply.github.com>
2022-05-04 16:52:50 -0700

    Update Push1test.ino

--- Push1test.ino
+++ Push1test.ino
@@ -1,9 +1,10 @@
-/// 5/4/2022 2:16 pm
+/// 5/4/2022 4:52 pm
 /// CPE 301 Swamp Cooler Project
 /// Written by: Jeremy Laporte, Nimisha Sabu, Yeamin Chowdery
 /// const int fan = 45; volatile const int yLED = 53; (B0)
 /// volatile const int bLED = 51; (B2) volatile const int rLED = 47; (L2) volatile const int gLED = 49; (L0)
 /// const int stepButton = 50 (B3); //volatile int onOffButton = 18 (D3); //volatile int resetButton = 38 (D7);
+/// stepButton = 43; ( )
 /// input: DDr = 0; inw/pullup: DDr = 0, port = 1, pin = 1; output: DDr = 1;
 
 #include <LiquidCrystal.h>  // LCD display library
@@ -20,9 +21,9 @@ LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
 Stepper myStepper(512, 2, 3, 4, 5);
 DHT dht(DHTPIN, DHTTYPE);
 
-unsigned char *port_B = (unsigned char *) 0x25;  // for yLED and bLED and stepButton
+unsigned char *port_B = (unsigned char *) 0x25;  // for yLED and bLED
 unsigned char *DDR_B = (unsigned char *) 0x24;
-unsigned char *port_L = (unsigned char *) 0x10B; // for fan rLED and gLED
+unsigned char *port_L = (unsigned char *) 0x10B; // for fan stepButton rLED and gLED
 unsigned char *DDR_L = (unsigned char *) 0x10A;
 unsigned char *port_D = (unsigned char *) 0x2B; // for onOff and reset button
 unsigned char *DDR_D = (unsigned char *) 0x2A;
@@ -45,16 +46,18 @@ volatile float tempIn = 0;      // temperature reading
 volatile float tempF = 0;       // temperature Farenheit value
 volatile float humidIn = 0;     // humidity reading
 volatile int disabledState = 1; // Error state variable
-int stepButtonState = 0;        
-int steppos = 0;
+int stepButtonState = 0;        // Stepper motor button High or low
 volatile int resetState = 0;    // Reset button/Esc-Error pressed?
 int count1 = 0;                 // Serial-single-print disabled flag
 int count2 = 0;                 // Serial-single-print idle flag
 int count3 = 0;                 // Serial-single-print error flag
 int count4 = 0;                 // Serial-single-print running flag
-void print_time();              // prototype the function for calling
-int resval = 0;                 // holds the sensor value
-int respin = A5;                // analog water sensor - pin used
+void print_time();
+unsigned int adc_read(unsigned char adc_channel_num);
+void adc_init();
+// prototype the function for calling
+unsigned int resval = 0;                 // holds the sensor value
+unsigned char respin = A5;                // analog water sensor - pin used
 
 
 void setup()
@@ -71,10 +74,10 @@ void setup()
   *DDR_B |= 0b00000001 | 0b00000100;              // yled | bled - output
   *DDR_L |= 0b00010000 | 0b00000001 | 0b00000100; // fan | gled | rled - output
   *DDR_D &= 0b01110111;                           // resetButton | onOffButton - input
-  *DDR_B &= 0b11110111;                           // stepMotor - input
+  *DDR_L |= 0b01000000;                           // stepMotor - input
 
   // 
-  *EICRA_1 |= 0b00000001;                         // set interrupt trigger to CHANGE
+  *EICRA_1 |= 0b00000010;                         // set interrupt trigger to CHANGE
   *EMISK_1 |= 0b00001000;                         // sets PD3 for the interrupt
   *SREG_1 |= 0b10000000;                          // sets initial status reg value
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // initially find and set the time and date
@@ -91,7 +94,9 @@ void loop() {
   humidIn = dht.readHumidity();
   tempIn = dht.readTemperature();
   tempF = dht.readTemperature(true);
-  resval = adc_read(respin);
+  resval = adc_read(5);
+  Serial.print(resval);
+  Serial.print('\n');
     
   if (disabledState==1){
    // DISABLED State
@@ -109,6 +114,8 @@ void loop() {
   
   else {
    // IDLE/RUNNING/ERROR State hub
+     Serial.print(resval);
+     Serial.print('\n');
     // During state transition, send this date and time once
     if (count2 == 0) {
       print_time();
@@ -118,10 +125,9 @@ void loop() {
     *port_B &= (0 << PINB0); // yellow led off
     *port_L |= (1 << PINL0); // turn the green led on
     // Stepper Motor, should move independent of rest, not when disabled
-    stepButtonState = digitalRead(53);
+    stepButtonState = digitalRead(43);
     if (stepButtonState == HIGH){
       myStepper.step(100); 
-      steppos += 1;
     }
     lcd.clear();
     lcd.display();
@@ -147,8 +153,8 @@ void loop() {
       }
       count1 = 0; count2 = 0; count3 = 1; count4 = 0;
       // Stepper Motor, should move independent of rest, not when disabled
-      stepButtonState = digitalRead(53);
-      if (stepButtonState == HIGH){myStepper.step(100); steppos += 1;}
+      stepButtonState = digitalRead(43);
+      if (stepButtonState == HIGH){myStepper.step(100);}
       
       resetState=!digitalRead(38);
       *port_L &= (0 << PINL0); //turn off the green light
@@ -172,12 +178,13 @@ void loop() {
       *port_B |= (1 << PINB2); // turn the blue led on
       *port_L |= (1 << PINL4); // turn the fan on
       // Stepper Motor, should move independent of rest, not when disabled
-      stepButtonState = digitalRead(53);
-      if (stepButtonState == HIGH){myStepper.step(100); steppos += 1;}
+      stepButtonState = digitalRead(43);
+      if (stepButtonState == HIGH){myStepper.step(100);}
       humidIn = dht.readHumidity();
       tempIn = dht.readTemperature();
       tempF = dht.readTemperature(true);
-      resval = adc_read(respin);
+      resval = adc_read(5);
+      Serial.print(resval);
       lcd.display();
       lcd.print(resval);
       lcd.clear();
@@ -264,7 +271,7 @@ unsigned int adc_read(unsigned char adc_channel_num)
   *my_ADMUX += adc_channel_num;  // set the channel selection bits
   *my_ADCSRA |= 0x40;  // set bit 6 of ADCSRA to 1 to start a conversion
   // wait for the conversion to complete
-  while (*my_ADCSRA & 0x10);
-   return (1000 - *my_ADC_DATA);  // return the result in the ADC data register
+  while ((*my_ADCSRA & 0x40) != 0);
+   return (*my_ADC_DATA);  // return the result in the ADC data register
 
 }
